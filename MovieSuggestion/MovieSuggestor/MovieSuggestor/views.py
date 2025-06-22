543064a4d8c652a182c28d8af0ab3efaf6ca5299
sabhya1290 <sabhya1290@gmail.com>
2025-06-22 11:40:43 +0530

    initial

---
+++ MovieSuggestion/MovieSuggestor/MovieSuggestor/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .forms import  UserRegistrationForm
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+
+def index(request):
+    return render(request,'index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request, user)
+            return redirect('index.html')
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'registration/register.html', {'form':form})
+    
